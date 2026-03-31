a9ed6c4f58896fe5362200b5dda51d376734d289
Manika Dahal <manikadahal8@gmail.com>
2026-03-31 07:09:49 +0545

    Fixed setiings.py for django admin

--- kissan_backend/kissan_core/settings.py
+++ kissan_backend/kissan_core/settings.py
@@ -138,8 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files finders
