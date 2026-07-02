8f6495bf158b809825faa5a3e0d0c1f37b2d21c2
Manika Dahal <manikadahal8@gmail.com>
2026-07-02 13:27:52 +0545

    Google login successful

--- kissan_connect/lib/features/auth/presentation/pages/signup_page.dart
+++ kissan_connect/lib/features/auth/presentation/pages/signup_page.dart
@@ -213,9 +213,9 @@ class _SignupPageState extends ConsumerState<SignupPage> {
                     setState(() => _isGoogleLoading = true);
                     try {
                       final GoogleSignIn googleSignIn = GoogleSignIn(
-                        serverClientId: '344012641554-kuche2uqq1mtm411l6eq2bu5ueadm2dd.apps.googleusercontent.com',
+                        serverClientId: '788697519956-v4sldco3nb8q4jqcqnl4gdmqu98i6qo7.apps.googleusercontent.com',
                       );
-                      
+                      await googleSignIn.signOut();
                       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
                       if (googleUser != null) {
                         final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
